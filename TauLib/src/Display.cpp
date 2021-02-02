6da48a26f09ee284a3e3a7e33c8618d471ac3d0e
Axanar <steve@iterator.com>
2021-02-01 20:26:57 -0500

    add more WinInit variations

--- TauLib/src/Display.cpp
+++ TauLib/src/Display.cpp
@@ -1,5 +1,6 @@
 #include "Display.h"
 #include <assert.h>
+#include "Tau_Rect.h"
 
 ///
 /// @file
@@ -10,33 +11,71 @@
 using namespace std;
 using namespace Tau;
 
-// A Display is a Win (Window) that takes over the entire display.
+// A Display is a Win (Window) that usually takes over the entire display.
 // There is no title bar or border as the window fills the entire display.
 // There are two options when creating the full screen window.
 // 1) SDL_WINDOW_FULLSCREEN and you specify a resolution of the display, the display switches to that resolution.  The window is the full screen at that resolution.
 // 2) SDL_WINDOW_FULLSCREEN_DESKTOP where you take over the entire display at the current resolution.
 
-bool Display::InitDisplay(unsigned int _displayIndex)                           ///< Uses flag SDL_WINDOW_FULLSCREEN_DESKTOP
+/// @brief Initilaizes the LCD Display using SDL_WINDOW_FULLSCREEN_DESKTOP
+bool Display::InitDisplay_FULLSCREEN_DESKTOP(unsigned int _displayIndex)
 {
     int specifyDisplayNum = (int)SDL_WINDOWPOS_UNDEFINED_DISPLAY(_displayIndex);
+    Tau_Rect bounds { { specifyDisplayNum, specifyDisplayNum }, { 0, 0 } };
 
-    bool ret = Win::InitWin(_displayIndex, "", { { specifyDisplayNum, specifyDisplayNum }, { 0, 0 } }, SDL_WINDOW_FULLSCREEN_DESKTOP,
-                                                                                                       SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    SDL_GetDisplayBounds(displayIndex, &displayBounds);
-    winRect = displayBounds;  // the window bounds is the display bounds as the window is the entire display screen
-    displayResolution = displayBounds.GetSize();
+    bool ret = Win::InitWin(_displayIndex, "", bounds, SDL_WINDOW_FULLSCREEN_DESKTOP,
+                                                       SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    InitDisplay_Common();
     return ret;
 }
 
-bool Display::InitDisplay(int _displayIndex, Tau_Size newResolution)   ///< Uses flag SDL_WINDOW_FULLSCREEN and passes the new resolution
-{
+/// @brief Initilaizes the LCD Display using SDL_WINDOW_FULLSCREEN_DESKTOP
+bool Display::InitDisplay_FULLSCREEN_DESKTOP(unsigned int _displayIndex, const std::string& _title, const Tau_Rect& bounds, Uint32 _flagsWin, Uint32 _flagsRenderer) {
     int specifyDisplayNum = (int)SDL_WINDOWPOS_UNDEFINED_DISPLAY(_displayIndex);
 
-    bool ret = Win::InitWin(_displayIndex, "", { { specifyDisplayNum, specifyDisplayNum }, newResolution }, SDL_WINDOW_FULLSCREEN,
-                                                                                                            SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    bool ret = Win::InitWin(_displayIndex, _title, bounds, SDL_WINDOW_FULLSCREEN_DESKTOP | _flagsWin, _flagsRenderer);
+
+    InitDisplay_Common();
+    return ret;
+}
+
+/// @brief Initilaizes the LCD Display using SDL_WINDOW_FULLSCREEN and passes the new resolution
+bool Display::InitDisplay_FULLSCREEN(unsigned int _displayIndex, Tau_Size newResolution)
+{
+    int specifyDisplayNum = (int)SDL_WINDOWPOS_UNDEFINED_DISPLAY(_displayIndex);
+    Tau_Rect bounds { { specifyDisplayNum, specifyDisplayNum }, newResolution };
+
+    bool ret = Win::InitWin(_displayIndex, "", bounds, SDL_WINDOW_FULLSCREEN,
+                                                       SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    InitDisplay_Common();
+    return ret;
+}
+
+/// @brief Initilaizes the LCD Display using SDL_WINDOW_FULLSCREEN and passes the new resolution
+bool Display::InitDisplay_FULLSCREEN(unsigned int _displayIndex, Tau_Size newResolution, const std::string& _title, Uint32 _flagsWin, Uint32 _flagsRenderer)
+{
+    int specifyDisplayNum = (int)SDL_WINDOWPOS_UNDEFINED_DISPLAY(_displayIndex);
+    Tau_Rect bounds { { specifyDisplayNum, specifyDisplayNum }, newResolution };
+
+    bool ret = Win::InitWin(_displayIndex, _title, bounds, SDL_WINDOW_FULLSCREEN | _flagsWin, _flagsRenderer);
+
+    InitDisplay_Common();
+    return ret;
+}
+
+/// @brief InitDisplay - passes all args directly to InitWin.  it might not be full screen.
+bool Display::InitDisplay(unsigned int _displayIndex, const std::string& _title, const Tau_Rect& bounds, Uint32 _flagsWin, Uint32 _flagsRenderer) {
+    bool ret = InitWin(_displayIndex, _title, bounds, _flagsWin, _flagsRenderer);
+    InitDisplay_Common();
+    return ret;
+}
+    
+/// @brief Perform some common calls at the end of the Init routines.
+void Display::InitDisplay_Common() {
     SDL_GetDisplayBounds(displayIndex, &displayBounds);
     winRect = displayBounds;  // the window bounds is the display bounds as the window is the entire display screen
     displayResolution = displayBounds.GetSize();
-    return ret;
 }
 
--- TauLib/src/Display.h
+++ TauLib/src/Display.h
@@ -10,7 +10,7 @@
 
 namespace Tau {
 
-/// @struct Display A Display is a Win (Window) that takes over the entire display.
+/// @struct Display is a Win (Window) that usually takes over the entire display.
 /// There is no title bar or border as the window fills the entire display.
 /// There are two options when creating the full screen window.
 /// 1) SDL_WINDOW_FULLSCREEN and you specify a resolution of the display, the display switches to that resolution.  The window is the full screen at that resolution.
@@ -21,17 +21,34 @@ namespace Tau {
 struct Display : public Win {
 
     Display() : Win() { }
-    
-    /// @brief InitDisplay Uses flag SDL_WINDOW_FULLSCREEN_DESKTOP and takes over the entire display at the current resolution.
-    bool InitDisplay(unsigned int _displayIndex);
 
-    /// @brief InitDisplay Uses flag SDL_WINDOW_FULLSCREEN and takes over the display at the new resolution
-    bool InitDisplay(int _displayIndex, Tau_Size newResolution);
+    /// @brief InitDisplay - Uses flag SDL_WINDOW_FULLSCREEN_DESKTOP and takes over the entire display at the current resolution.
+    bool InitDisplay_FULLSCREEN_DESKTOP(unsigned int _displayIndex);
+
+    /// @brief InitDisplay Uses flag SDL_WINDOW_FULLSCREEN_DESKTOP and takes over the entire display at the current resolution.
+    /// @note SDL_WINDOW_FULLSCREEN_DESKTOP is OR'd into _flagsWin
+    bool InitDisplay_FULLSCREEN_DESKTOP(unsigned int _displayIndex, const std::string& _title, const Tau_Rect& bounds, Uint32 _flagsWin, Uint32 _flagsRenderer);
+
+    /// @brief InitDisplay - Uses flag SDL_WINDOW_FULLSCREEN and takes over the display at the new resolution
+    bool InitDisplay_FULLSCREEN(unsigned int _displayIndex, Tau_Size newResolution);
+
+    /// @brief InitDisplay - Uses flag SDL_WINDOW_FULLSCREEN and takes over the display at the new resolution
+    /// @note SDL_WINDOW_FULLSCREEN is OR'd into _flagsWin
+    bool InitDisplay_FULLSCREEN(unsigned int _displayIndex, Tau_Size newResolution, const std::string& _title, Uint32 _flagsWin, Uint32 _flagsRenderer);
+
+    /// @brief Return a copy of the Display DrawArea
+    DrawArea GetDrawArea() { return DrawArea(renderer, winRect); }
 
     Tau_Rect displayBounds;
     Tau_Size displayResolution;
 
     static int GetNumberOfDisplays() { return SDL_GetNumVideoDisplays(); }
+
+    /// @brief InitDisplay - passes all args directly to InitWin.  it might not be full screen.
+    bool InitDisplay(unsigned int _displayIndex, const std::string& _title, const Tau_Rect& bounds, Uint32 _flagsWin, Uint32 _flagsRenderer);
+    
+private:
+    void InitDisplay_Common();
 };
 
 }
