e39786ef253349fae374e92a0986db00f30ea0f3
Axanar <steve@iterator.com>
2021-05-01 11:33:42 -0400

    add current time and datetime string format routines

--- TauLib/src/Tau_Util.cpp
+++ TauLib/src/Tau_Util.cpp
@@ -7,21 +7,88 @@ namespace Tau {
                 //*******************************
                 //           Sleep
                 //*******************************
+//
+// CurrentTimeAsTime_t
+//
+time_t CurrentTimeAsTime_t()
+{
+    auto current = chrono::system_clock::now();
+    time_t now = chrono::system_clock::to_time_t(current);
+    return now;
+}
 
+//
+// CurrentTimeAsTime_point
+//
+chrono::system_clock::time_point CurrentTimeAsTime_point()
+{
+    auto current = chrono::system_clock::now();
+    return current;
+}
+
+//
+// Time_t_ToString
+//
+string Time_t_ToString(time_t t, const string& format)
+{
+    if (t == 0)
+        t = CurrentTimeAsTime_t();
+
+    char buf[200];
+    if (format != "") {
+        struct tm tm_buf;
+        auto err_no = localtime_s(&tm_buf, &t);
+        if (err_no == 0) {
+            if (strftime(buf, sizeof(buf), format.c_str(), &tm_buf))
+                return buf;
+            else {
+                ctime_s(buf, sizeof(buf), &t);   // error with the format, use default
+                return buf;
+            }
+        } else {
+            ctime_s(buf, sizeof(buf), &t);   // error with the format, use default
+            return buf;
+        }
+    } else {
+        ctime_s(buf, sizeof(buf), &t);   // error with the format, use default
+        return buf;
+    }
+}
+
+//
+// CurrentTime_TosString
+//
+string CurrentTime_TosString(const string& format)
+{
+    return Time_t_ToString(CurrentTimeAsTime_t(), format);
+}
+
+//
+// Sleep_Minutes
+//
 void Sleep_Minutes(int delay) {
-    std::this_thread::sleep_for(std::chrono::minutes(delay));
+    this_thread::sleep_for(chrono::minutes(delay));
 }
 
+//
+// Sleep_Seconds
+//
 void Sleep_Seconds(int delay) {
-    std::this_thread::sleep_for(std::chrono::seconds(delay));
+    this_thread::sleep_for(chrono::seconds(delay));
 }
 
+//
+// Sleep_MilliSeconds
+//
 void Sleep_MilliSeconds(int delay) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+    this_thread::sleep_for(chrono::milliseconds(delay));
 }
 
+//
+// Sleep_MicroSeconds
+//
 void Sleep_MicroSeconds(int delay) {
-    std::this_thread::sleep_for(std::chrono::microseconds(delay));
+    this_thread::sleep_for(chrono::microseconds(delay));
 }
 
 }
--- TauLib/src/Tau_Util.h
+++ TauLib/src/Tau_Util.h
@@ -4,9 +4,19 @@
 #include <chrono>
 #include <vector>
 #include <string>
+#include <ctime>
 
 namespace Tau {
 
+// get current time
+time_t CurrentTimeAsTime_t();
+std::chrono::system_clock::time_point CurrentTimeAsTime_point();
+
+// time to string
+std::string Time_t_ToString(time_t t=0, const std::string& format="");
+std::string CurrentTime_TosString(const std::string& format="");
+
+// delay routines
 void Sleep_Minutes(int delay);
 void Sleep_Seconds(int delay);
 void Sleep_MilliSeconds(int delay);
