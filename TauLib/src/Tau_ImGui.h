e7724d8e26cf32e597324ed19cf651d3dd772baf
Axanar <steve@iterator.com>
2022-07-06 21:30:10 -0400

    added Tau_ImGui.* to TauLib and added ImGui_ShowDemoWindow()

---
+++ TauLib/src/Tau_ImGui.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "SDL_Shared.h"
+
+///
+/// @brief namespace Tau - avoid conflict with other libraries
+///
+namespace Tau { // to avoid conflict with other libraries
+
+///
+/// @brief ImGui_ShowDemoWindow - calls ImGui::ShowDemoWindow() from Tau
+///
+void ImGui_ShowDemoWindow(SDL_Shared<SDL_Window> window, SDL_Shared<SDL_Renderer> renderer);
+}
