ecfb3b1fb8462ea9edaeff3eb2d0409429d2a150
Axanar <steve@iterator.com>
2022-07-06 22:32:15 -0400

    added ImGui_Init() and ImGui_Cleanup()

--- TauLib/src/Tau_ImGui.h
+++ TauLib/src/Tau_ImGui.h
@@ -8,6 +8,16 @@
 namespace Tau { // to avoid conflict with other libraries
 
 ///
+/// @brief ImGui_Init
+///
+void ImGui_Init(SDL_Shared<SDL_Window> window, SDL_Shared<SDL_Renderer> renderer);
+
+///
+/// @brief ImGui_Cleanup
+///
+void ImGui_Cleanup();
+
+///
 /// @brief ImGui_ShowDemoWindow - calls ImGui::ShowDemoWindow() from Tau
 ///
 void ImGui_ShowDemoWindow(SDL_Shared<SDL_Window> window, SDL_Shared<SDL_Renderer> renderer);
